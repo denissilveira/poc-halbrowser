779a783d3e8897db62bbbf42c873c465097862e2
Denis Silveira <denis.rayan@gmail.com>
2018-07-20 19:39:27 +0100

    Poc Halbrowser

---
+++ src/main/java/com/poc/halbrowser/HalbrowserApplication.java
@@ -0,0 +1,12 @@
+package com.poc.halbrowser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalbrowserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HalbrowserApplication.class, args);
+    }
+}
---
+++ src/test/java/com/poc/halbrowser/HalbrowserApplicationTests.java
@@ -0,0 +1,16 @@
+package com.poc.halbrowser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HalbrowserApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
